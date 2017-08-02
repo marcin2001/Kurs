757c705faf80193357fa9dc1e81d4bcb55a72517
Myroslav <forvaro@gmail.com>
2017-08-02 08:04:52 +0000

    htmk

--- proscript.js
+++ proscript.js
@@ -74,9 +74,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"h1"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується як заголовок.';
 			var sor = document.getElementsByTagName('source')
-			sor[0].src = "video/h1.mp4";	
-			sor[1].src = "video/h1.ogg";
-			sor[2].src = "video/h1.webm";			
+			sor[0].src = "video/h1-html.mp4";	
+			sor[1].src = "video/h1-html.ogg";
+			sor[2].src = "video/h1-html.webm";			
 			video.load();
 		}
 
@@ -85,9 +85,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"p"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується як абзац.';
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/p.mp4";
-			sor[1].src = "video/p.ogg";
-			sor[2].src = "video/p.webm";			
+			sor[0].src = "video/p-html.mp4";
+			sor[1].src = "video/p-html.ogg";
+			sor[2].src = "video/p-html.webm";			
 			video.load();
 		}
 		
@@ -96,9 +96,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"br"' + '</b>';
 			theory.innerHTML = 'Являється одиночним тегом, використовується для переносу' + '<br>' + 'строфи в інший рядок.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/br.mp4";
-			sor[1].src = "video/br.ogg";
-			sor[2].src = "video/br.webm";			
+			sor[0].src = "video/br-html.mp4";
+			sor[1].src = "video/br-html.ogg";
+			sor[2].src = "video/br-html.webm";			
 			video.load();
 		}
 		
@@ -106,9 +106,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"hr"' + '</b>';
 			theory.innerHTML = 'Являється одиночним тегом, використовується' + '<br>' + 'як горизонтальна лінія.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/hr.mp4";
-			sor[1].src = "video/hr.ogg";
-			sor[2].src = "video/hr.webm";			
+			sor[0].src = "video/hr-html.mp4";
+			sor[1].src = "video/hr-html.ogg";
+			sor[2].src = "video/hr-html.webm";			
 			video.load();
 		}
 		
@@ -116,9 +116,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"ul + li"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'як невпорядкований список.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/ul+li.mp4";
-			sor[1].src = "video/ul+li.ogg";
-			sor[2].src = "video/ul+li.webm";			
+			sor[0].src = "video/ul+li-html.mp4";
+			sor[1].src = "video/ul+li-html.ogg";
+			sor[2].src = "video/ul+li-html.webm";			
 			video.load();
 		}
 		
@@ -126,9 +126,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"ol + li"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'як впорядкований список.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/ol+li.mp4";
-			sor[1].src = "video/ol+li.ogg";
-			sor[2].src = "video/ol+li.webm";			
+			sor[0].src = "video/ol+li-html.mp4";
+			sor[1].src = "video/ol+li-html.ogg";
+			sor[2].src = "video/ol+li-html.webm";			
 			video.load();
 		}
 		
@@ -136,9 +136,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"sup"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'як верхній індекс.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/sup.mp4";
-			sor[1].src = "video/sup.ogg";
-			sor[2].src = "video/sup.webm";			
+			sor[0].src = "video/sup-html.mp4";
+			sor[1].src = "video/sup-html.ogg";
+			sor[2].src = "video/sup-html.webm";			
 			video.load();
 		}
 		
@@ -146,9 +146,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"sub"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'як нижній індекс.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/sub.mp4";
-			sor[1].src = "video/sub.ogg";
-			sor[2].src = "video/sub.webm";			
+			sor[0].src = "video/sub-html.mp4";
+			sor[1].src = "video/sub-html.ogg";
+			sor[2].src = "video/sub-html.webm";			
 			video.load();
 		}
 		
@@ -156,9 +156,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"i"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'для виділення тексту.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/i.mp4";
-			sor[1].src = "video/i.ogg";
-			sor[2].src = "video/i.webm";			
+			sor[0].src = "video/i-html.mp4";
+			sor[1].src = "video/i-html.ogg";
+			sor[2].src = "video/i-html.webm";			
 			video.load();
 		}
 		
@@ -166,9 +166,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"b"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'для виділення тексту жирним шрифтом.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/b.mp4";
-			sor[1].src = "video/b.ogg";
-			sor[2].src = "video/b.webm";			
+			sor[0].src = "video/b-html.mp4";
+			sor[1].src = "video/b-html.ogg";
+			sor[2].src = "video/b-html.webm";			
 			video.load();
 		}
 		
@@ -176,9 +176,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"em"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'для виділення тексту з особливим акцентом.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/em.mp4";
-			sor[1].src = "video/em.ogg";
-			sor[2].src = "video/em.webm";			
+			sor[0].src = "video/em-html.mp4";
+			sor[1].src = "video/em-html.ogg";
+			sor[2].src = "video/em-html.webm";			
 			video.load();
 		}
 		
@@ -186,9 +186,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"strong"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'для виділення важливого тексту.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/strong.mp4";
-			sor[1].src = "video/strong.ogg";
-			sor[2].src = "video/strong.webm";			
+			sor[0].src = "video/strong-html.mp4";
+			sor[1].src = "video/strong-html.ogg";
+			sor[2].src = "video/strong-html.webm";			
 			video.load();
 		}
 		
@@ -196,9 +196,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"mark"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, використовується' + '<br>' + 'для простого виділення тексту.';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/mark.mp4";
-			sor[1].src = "video/mark.ogg";
-			sor[2].src = "video/mark.webm";		
+			sor[0].src = "video/mark-html.mp4";
+			sor[1].src = "video/mark-html.ogg";
+			sor[2].src = "video/mark-html.webm";		
 			video.load();
 		}
 		
@@ -206,9 +206,9 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"del"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, виділяє тест' + '<br>' + 'який був видалений в нові версії докуиентаю';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/del.mp4";
-			sor[1].src = "video/del.ogg";
-			sor[2].src = "video/del.webm";		
+			sor[0].src = "video/del-html.mp4";
+			sor[1].src = "video/del-html.ogg";
+			sor[2].src = "video/del-html.webm";		
 			video.load();
 		}
 		
@@ -216,8 +216,8 @@
 			title.innerHTML = 'Тег ' + '<b>' + '"ins"' + '</b>';
 			theory.innerHTML = 'Являється закриваючим тегом, виділяє тест' + '<br>' + 'який був добавлений в нові версії докуиентаю';	
 			var sor = document.getElementsByTagName('source');
-			sor[0].src = "video/ins.mp4";
-			sor[1].src = "video/ins.ogg";
-			sor[2].src = "video/ins.webm";		
+			sor[0].src = "video/ins-html.mp4";
+			sor[1].src = "video/ins-html.ogg";
+			sor[2].src = "video/ins-html.webm";		
 			video.load();
 		}
